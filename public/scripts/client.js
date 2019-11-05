15e3607c73d156d0aa60b0e62fd9781f437d419d
james-truong <james.truong@hotmail.com>
2019-11-05 21:24:44 +0000

    added AJAX requests and form validation

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,71 +1,130 @@
 // Test / driver code (temporary). Eventually will get this from the server.
+
+const renderTweets = (tweets) => {
+
+    //clear the container before to read all tweets
+    $("#tweets-container").empty();
+
+    // loops through tweets from newer to older
+    for (let i in tweets) {
+
+        // calls createTweetElement for each tweet
+        $tweet = createTweetElement(tweets[i]);
+
+        // takes return value and appends it to the tweets container
+        $('#tweets-container').prepend($tweet);
+    }
+
+}
+
+function createTweetElement(data) {
+    $dateCreated = new Date(data.created_at);
+    $dateToday = new Date();
+
+    $timeDiff = Math.abs($dateToday.getTime() - $dateCreated.getTime());
+    $diffDays = Math.ceil($timeDiff / (1000 * 3600 * 24));
+    $tweet = (`<article>` +
+        `<header>` +
+        `<img src="${data.user.avatars}">` +
+        `<h2>${data.user.name}</h2>` +
+        `<span>${data.user.handle}</span>` +
+        `</header>` +
+        `<div class="body">${data.content.text}</div>` +
+        `<footer>` +
+        `<span class="daysAgo">${$diffDays} days ago</span>` +
+        `<div class="options">` +
+        `<span><i class="fa fa-flag" aria-hidden="true"></i></span>` +
+        `<span><i class="fa fa-retweet" aria-hidden="true"></i></span>` +
+        `<span><i class="fa fa-heart" aria-hidden="true"></i></span>` +
+        `</div>` +
+        `</footer>` +
+        `</article>`);
+    $('#tweets-container').prepend($tweet);
+}
+
+$("#add").on("click", function(event) {
+
+    //prevent to change the page
+    event.preventDefault();
+
+    //get data from the form
+    $textarea = $(this).closest("form").find("textarea");
+    $message = $(this).closest("form").find("#message");
+    $counter = $(this).closest("form").find(".counter");
+    $data = $textarea.serialize();
+
+    $text = $textarea.val().trim();
+    $textLength = $text.length;
+
+
+    if ($text === "" || $text === null) {
+        $message.text("Your message is empty!").toggle(true);
+        $textarea.focus();
+
+    } else if ($textLength > 140) {
+        $message.text("Your message is too long!").toggle(true);
+        $textarea.focus();
+
+    } else {
+        // proceed as normally
+        $.post("/tweets/", $data)
+            .done(function() {
+                loadTweets();
+            });
+
+        //hidden the message if it is shown, clear the textarea, and reset the char-counter
+        $message.text("").toggle(false);
+        $textarea.val("").focus();
+        $counter.text("140");
+    }
+
+});
+
 $(document).ready(function() {
-    const tweetData = [{
-            "user": {
-                "name": "Newton",
-                "avatars": "https://i.imgur.com/73hZDYK.png",
-                "handle": "@SirIsaac"
-            },
-            "content": {
-                "text": "If I have seen further it is by standing on the shoulders of giants"
-            },
-            "created_at": 1461116232227
-        },
-        {
-            "user": {
-                "name": "Descartes",
-                "avatars": "https://i.imgur.com/nlhLi3I.png",
-                "handle": "@rd"
-            },
-            "content": {
-                "text": "Je pense , donc je suis"
-            },
-            "created_at": 1461113959088
-        }
-    ];
-    const renderTweets = (tweets) => {
-
-        //clear the container before to read all tweets
-        $("#tweets-container").empty();
-
-        // loops through tweets from newer to older
-        for (let i in tweets) {
-
-            // calls createTweetElement for each tweet
-            $tweet = createTweetElement(tweets[i]);
-
-            // takes return value and appends it to the tweets container
-            $('#tweets-container').prepend($tweet);
-        }
-
+    function loadTweets() {
+        $.ajax('/tweets', { method: 'GET' })
+            .then(function(result) {
+                renderTweets(result)
+            });
     }
+    $("#add").on("click", function(event) {
 
-    function createTweetElement(data) {
-        $dateCreated = new Date(data.created_at);
-        $dateToday = new Date();
+        //prevent to change the page
+        event.preventDefault();
 
-        $timeDiff = Math.abs($dateToday.getTime() - $dateCreated.getTime());
-        $diffDays = Math.ceil($timeDiff / (1000 * 3600 * 24));
-        $tweet = (`<article>` +
-            `<header>` +
-            `<img src="${data.user.avatars}">` +
-            `<h2>${data.user.name}</h2>` +
-            `<span>${data.user.handle}</span>` +
-            `</header>` +
-            `<div class="body">${data.content.text}</div>` +
-            `<footer>` +
-            `<span class="daysAgo">${$diffDays} days ago</span>` +
-            `<div class="options">` +
-            `<span><i class="fa fa-flag" aria-hidden="true"></i></span>` +
-            `<span><i class="fa fa-retweet" aria-hidden="true"></i></span>` +
-            `<span><i class="fa fa-heart" aria-hidden="true"></i></span>` +
-            `</div>` +
-            `</footer>` +
-            `</article>`);
-        $('#tweets-container').append($tweet);
-    }
+        //get data from the form
+        $textarea = $(this).closest("form").find("textarea");
+        $message = $(this).closest("form").find("#message");
+        $counter = $(this).closest("form").find(".counter");
+        $data = $textarea.serialize();
 
-    renderTweets(tweetData);
+        $text = $textarea.val().trim();
+        $textLength = $text.length;
 
+
+        if ($text === "" || $text === null) {
+            $message.text("Your message is empty!").toggle(true);
+            $textarea.focus();
+
+        } else if ($textLength > 140) {
+            $message.text("Your message is too long!").toggle(true);
+            $textarea.focus();
+
+        } else {
+            // proceed as normally
+            $.post("/tweets/", $data)
+                .done(function() {
+                    loadTweets();
+                });
+
+            //hidden the message if it is shown, clear the textarea, and reset the char-counter
+            $message.text("").toggle(false);
+            $textarea.val("").focus();
+            $counter.text("140");
+        }
+
+    });
+
+    loadTweets();
     // Test / driver code (temporary)
 });
